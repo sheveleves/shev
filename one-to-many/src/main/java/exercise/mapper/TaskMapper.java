01c59a2e61273fd939120484f192ec697aa2fd84
sheveleves <shevelve@gmail.com>
2023-12-01 18:13:31 +0600

    update test passes successfully

---
+++ one-to-many/src/main/java/exercise/mapper/TaskMapper.java
@@ -0,0 +1,31 @@
+package exercise.mapper;
+
+import exercise.dto.TaskCreateDTO;
+import exercise.dto.TaskDTO;
+import exercise.dto.TaskUpdateDTO;
+import exercise.model.Task;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskMapper {
+
+    // BEGIN
+    @Mapping(target = "assignee.id", source = "assigneeId")
+    public abstract Task map(TaskCreateDTO taskCreateDTO);
+    @Mapping(target = "assigneeId", source = "assignee.id")
+    public abstract TaskDTO map(Task task);
+//    @Mapping(target = "assignee.id", source = "assigneeId")
+    public abstract void update(TaskUpdateDTO taskUpdateDTO, @MappingTarget Task task);
+
+    // END
+
+}
